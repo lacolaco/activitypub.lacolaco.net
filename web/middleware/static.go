e5c762fb641813c059f19f20817a71558f2aa459
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-02-12 21:41:22 +0900

    fix: no cache index.html

--- web/middleware/static.go
+++ web/middleware/static.go
@@ -12,12 +12,18 @@ func Static(prefix, dir string) gin.HandlerFunc {
 		staticFS := static.LocalFile(dir, false)
 		url := c.Request.URL.Path
 		if staticFS.Exists(prefix, url) {
-			c.Header("Cache-Control", "public, must-revalidate, max-age=0")
+			if url == "/" {
+				// index.html is not cacheable
+				c.Header("Cache-Control", "no-cache")
+			} else {
+				c.Header("Cache-Control", "public, must-revalidate, max-age=0")
+			}
 			c.FileFromFS(url, staticFS)
 			return
 		}
 		c.Next()
 		if c.Writer.Status() == http.StatusNotFound {
+			c.Header("Cache-Control", "no-cache")
 			c.Status(http.StatusOK)
 			c.FileFromFS("/", staticFS)
 		}
--- web/middleware/static_test.go
+++ web/middleware/static_test.go
@@ -19,6 +19,9 @@ func TestStatic(t *testing.T) {
 		if w.Code != http.StatusOK {
 			tt.Errorf("got %d, want %d", w.Code, http.StatusOK)
 		}
+		if w.Result().Header.Get("Cache-Control") != "public, must-revalidate, max-age=0" {
+			tt.Errorf("got %s, want %s", w.Result().Header.Get("Cache-Control"), "no-cache")
+		}
 	})
 
 	t.Run("can serve index.html with /", func(tt *testing.T) {
@@ -32,6 +35,26 @@ func TestStatic(t *testing.T) {
 		}
 		if w.Result().Header.Get("Content-Type") != "text/html; charset=utf-8" {
 			tt.Errorf("got %s, want %s", w.Result().Header.Get("Content-Type"), "text/html; charset=utf-8")
+		}
+		if w.Result().Header.Get("Cache-Control") != "no-cache" {
+			tt.Errorf("got %s, want %s", w.Result().Header.Get("Cache-Control"), "no-cache")
+		}
+	})
+
+	t.Run("can serve unknown path with /", func(tt *testing.T) {
+		router := gin.New()
+		router.Use(middleware.Static("/", "../fixtures/static"))
+		req, _ := http.NewRequest("GET", "/unknown", nil)
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+		if w.Code != http.StatusOK {
+			tt.Errorf("got %d, want %d", w.Code, http.StatusOK)
+		}
+		if w.Result().Header.Get("Content-Type") != "text/html; charset=utf-8" {
+			tt.Errorf("got %s, want %s", w.Result().Header.Get("Content-Type"), "text/html; charset=utf-8")
+		}
+		if w.Result().Header.Get("Cache-Control") != "no-cache" {
+			tt.Errorf("got %s, want %s", w.Result().Header.Get("Cache-Control"), "no-cache")
 		}
 	})
 
