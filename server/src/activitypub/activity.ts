e3c03c5478a8b1e8bd63be43e802f8d4126e176a
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-07-13 09:27:17 +0900

    fix: add to,published to accept

--- server/src/activitypub/activity.ts
+++ server/src/activitypub/activity.ts
@@ -1,21 +1,22 @@
 import { AP, isType } from '@activity-kit/types';
 import { contextURIs } from './context';
+import { getID } from './utilities';
 
-export type Activity = AP.Activity;
+export type Activity = AP.Activity & Record<string, unknown>;
 
-export type FollowActivity = AP.Follow;
+export type FollowActivity = Activity & AP.Follow;
 
 export function isFollowActivity(object: unknown): object is FollowActivity {
   return isType<AP.Follow>(object, AP.ActivityTypes.FOLLOW);
 }
 
-export type UndoActivity = AP.Undo;
+export type UndoActivity = Activity & AP.Undo;
 
 export function isUndoActivity(object: unknown): object is UndoActivity {
   return isType<AP.Undo>(object, AP.ActivityTypes.UNDO);
 }
 
-export type AcceptActivity = AP.Accept;
+export type AcceptActivity = Activity & AP.Accept;
 
 export function isAcceptActivity(object: unknown): object is AcceptActivity {
   return isType<AP.Accept>(object, AP.ActivityTypes.ACCEPT);
@@ -29,5 +30,7 @@ export function buildAcceptAcivity(actorID: URL, object: Activity) {
     id,
     actor: actorID,
     object: object,
+    to: [getID(object.actor)],
+    published: new Date().toISOString(),
   } satisfies AcceptActivity;
 }
