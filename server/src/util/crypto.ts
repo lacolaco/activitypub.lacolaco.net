6491cecbc2d4804fc83369f0eaa514527d108d83
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-07-13 20:07:22 +0900

    fix: change publicKey type

--- server/src/util/crypto.ts
+++ server/src/util/crypto.ts
@@ -2,5 +2,5 @@ import { createPublicKey } from 'node:crypto';
 
 export function getPublicKey(privateKey: string) {
   const publicKey = createPublicKey(privateKey);
-  return publicKey.export({ type: 'spki', format: 'pem' }).toString();
+  return publicKey.export({ type: 'pkcs1', format: 'pem' }).toString();
 }
