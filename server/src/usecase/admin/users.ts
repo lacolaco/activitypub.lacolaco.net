07ca7642c1215fcab5329a1d99bc03cd5b53be7e
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-07-16 23:01:32 +0900

    fix: delete redundant span.end

--- server/src/usecase/admin/users.ts
+++ server/src/usecase/admin/users.ts
@@ -3,27 +3,19 @@ import { runInSpan } from '@app/tracing';
 
 export async function getUsers() {
   return runInSpan('admin.getUsers', async (span) => {
-    try {
-      const userRepo = new UsersRepository();
-      const users = await userRepo.getUsers();
-      return users;
-    } finally {
-      span.end();
-    }
+    const userRepo = new UsersRepository();
+    const users = await userRepo.getUsers();
+    return users;
   });
 }
 
 export async function getUserByUsername(hostname: string, username: string) {
   return runInSpan('admin.getUserByUsername', async (span) => {
-    try {
-      const userRepo = new UsersRepository();
-      const user = await userRepo.findByUsername(hostname, username);
-      if (user == null) {
-        return null;
-      }
-      return user;
-    } finally {
-      span.end();
+    const userRepo = new UsersRepository();
+    const user = await userRepo.findByUsername(hostname, username);
+    if (user == null) {
+      return null;
     }
+    return user;
   });
 }
